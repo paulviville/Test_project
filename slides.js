9544f795fe42306a21816f5b1121f71480833b0c
paulviville <paul.viville@gmail.com>
2020-11-18 00:33:30 +0100

    bug fixed

--- slides.js
+++ slides.js
@@ -13,262 +13,247 @@ import { loop } from './CMapJS/Modeling/Subdivision/Surface/Loop.js';
 import { butterfly } from './CMapJS/Modeling/Subdivision/Surface/Butterfly.js';
 
 export let cc_slide = new Slide(
-	function(domElement){
-			let width = domElement.parentElement.clientWidth;
-			let height = domElement.parentElement.clientHeight;
+	function(domElement, width, height){
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x191919);
+		this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
+		let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
+		pointLight0.position.set(10,8,5);
+		this.scene.add(pointLight0);
 
-			this.scene = new THREE.Scene();
-			this.scene.background = new THREE.Color(0x191919);
-			this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
-			let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
-			pointLight0.position.set(10,8,5);
-			this.scene.add(pointLight0);
+		this.renderer = new THREE.WebGLRenderer();	
+		this.renderer.setSize( width *0.8, height *0.8);
+		domElement.appendChild( this.renderer.domElement );
 
-			this.renderer = new THREE.WebGLRenderer();	
-			this.renderer.setSize( width *0.8, height *0.8);
-			domElement.appendChild( this.renderer.domElement );
+		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 2);
 
-			this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
-			this.camera.position.set(0, 0, 2);
+		let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
+		orbit_controls.enablePan = false;
+		orbit_controls.update();
 
-			let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
-			orbit_controls.enablePan = false;
-			orbit_controls.update();
+		
+		this.cmap2 = load_cmap2('off', cube_off);
+		this.map_renderer = new Renderer(this.cmap2);
+		this.map_renderer.vertices.create().add(this.scene);
+		this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
+		this.map_renderer.faces.create().add(this.scene);
 
-			
-			this.cmap2 = load_cmap2('off', cube_off);
-			this.map_renderer = new Renderer(this.cmap2);
-			this.map_renderer.vertices.create().add(this.scene);
-			this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
-			this.map_renderer.faces.create().add(this.scene);
-
-			function onKeyUp(event){
-				switch(event.which){
-					case 67:
-						catmull_clark(this.cmap2);
-						this.map_renderer.vertices.update();
-						this.map_renderer.edges.update();
-						this.map_renderer.faces.update();
-						break;
-					default:
-				}
+		function onKeyUp(event){
+			switch(event.which){
+				case 67:
+					catmull_clark(this.cmap2);
+					this.map_renderer.vertices.update();
+					this.map_renderer.edges.update();
+					this.map_renderer.faces.update();
+					break;
+				default:
 			}
-			domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
+		}
+		domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
 
-			this.running = false;
-			this.loop = function(){
-				if(this.running){
-					this.renderer.render(this.scene, this.camera);
-					requestAnimationFrame(this.loop.bind(this));
-				}
+		this.running = false;
+		this.loop = function(){
+			if(this.running){
+				this.renderer.render(this.scene, this.camera);
+				requestAnimationFrame(this.loop.bind(this));
 			}
+		}
 	}
 );
 
 export let ds_slide = new Slide(
-	function(domElement){
-			let width = domElement.parentElement.clientWidth;
-			let height = domElement.parentElement.clientHeight;
+	function(domElement, width, height){
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x191919);
+		this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
+		let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
+		pointLight0.position.set(10,8,5);
+		this.scene.add(pointLight0);
+		
+		this.renderer = new THREE.WebGLRenderer();	
+		this.renderer.setSize( width *0.8, height *0.8);
+		domElement.appendChild( this.renderer.domElement );
 
-			this.scene = new THREE.Scene();
-			this.scene.background = new THREE.Color(0x191919);
-			this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
-			let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
-			pointLight0.position.set(10,8,5);
-			this.scene.add(pointLight0);
-			
-			this.renderer = new THREE.WebGLRenderer();	
-			this.renderer.setSize( width *0.8, height *0.8);
-			domElement.appendChild( this.renderer.domElement );
+		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 2);
 
-			this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
-			this.camera.position.set(0, 0, 2);
+		let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
+		orbit_controls.enablePan = false;
+		orbit_controls.update();
 
-			let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
-			orbit_controls.enablePan = false;
-			orbit_controls.update();
+		
+		this.cmap2 = load_cmap2('off', cube_off);
+		this.map_renderer = new Renderer(this.cmap2);
+		this.map_renderer.vertices.create().add(this.scene);
+		this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
+		this.map_renderer.faces.create().add(this.scene);
 
-			
-			this.cmap2 = load_cmap2('off', cube_off);
-			this.map_renderer = new Renderer(this.cmap2);
-			this.map_renderer.vertices.create().add(this.scene);
-			this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
-			this.map_renderer.faces.create().add(this.scene);
-
-			function onKeyUp(event){
-				switch(event.which){
-					case 67:
-						doo_sabin(this.cmap2);
-						this.map_renderer.vertices.update();
-						this.map_renderer.edges.update();
-						this.map_renderer.faces.update();
-						break;
-					default:
-				}
+		function onKeyUp(event){
+			switch(event.which){
+				case 67:
+					doo_sabin(this.cmap2);
+					this.map_renderer.vertices.update();
+					this.map_renderer.edges.update();
+					this.map_renderer.faces.update();
+					break;
+				default:
 			}
-			domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
+		}
+		domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
 
-			this.loop = function(){
-				if(this.running){
-					this.renderer.render(this.scene, this.camera);
-					requestAnimationFrame(this.loop.bind(this));
-				}
+		this.loop = function(){
+			if(this.running){
+				this.renderer.render(this.scene, this.camera);
+				requestAnimationFrame(this.loop.bind(this));
 			}
+		}
 	}
 );
 
 export let sqrt2_slide = new Slide(
-	function(domElement){
-			let width = domElement.parentElement.clientWidth;
-			let height = domElement.parentElement.clientHeight;
+	function(domElement, width, height){
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x191919);
+		this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
+		let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
+		pointLight0.position.set(10,8,5);
+		this.scene.add(pointLight0);
+		
+		this.renderer = new THREE.WebGLRenderer();	
+		this.renderer.setSize( width *0.8, height *0.8);
+		domElement.appendChild( this.renderer.domElement );
 
-			this.scene = new THREE.Scene();
-			this.scene.background = new THREE.Color(0x191919);
-			this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
-			let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
-			pointLight0.position.set(10,8,5);
-			this.scene.add(pointLight0);
-			
-			this.renderer = new THREE.WebGLRenderer();	
-			this.renderer.setSize( width *0.8, height *0.8);
-			domElement.appendChild( this.renderer.domElement );
+		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 2);
 
-			this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
-			this.camera.position.set(0, 0, 2);
+		let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
+		orbit_controls.enablePan = false;
+		orbit_controls.update();
 
-			let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
-			orbit_controls.enablePan = false;
-			orbit_controls.update();
+		
+		this.cmap2 = load_cmap2('off', cube_off);
+		this.map_renderer = new Renderer(this.cmap2);
+		this.map_renderer.vertices.create().add(this.scene);
+		this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
+		this.map_renderer.faces.create().add(this.scene);
 
-			
-			this.cmap2 = load_cmap2('off', cube_off);
-			this.map_renderer = new Renderer(this.cmap2);
-			this.map_renderer.vertices.create().add(this.scene);
-			this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
-			this.map_renderer.faces.create().add(this.scene);
-
-			function onKeyUp(event){
-				switch(event.which){
-					case 67:
-						sqrt2(this.cmap2);
-						this.map_renderer.vertices.update();
-						this.map_renderer.edges.update();
-						this.map_renderer.faces.update();
-						break;
-					default:
-				}
+		function onKeyUp(event){
+			switch(event.which){
+				case 67:
+					sqrt2(this.cmap2);
+					this.map_renderer.vertices.update();
+					this.map_renderer.edges.update();
+					this.map_renderer.faces.update();
+					break;
+				default:
 			}
-			domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
+		}
+		domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
 
-			this.loop = function(){
-				if(this.running){
-					this.renderer.render(this.scene, this.camera);
-					requestAnimationFrame(this.loop.bind(this));
-				}
+		this.loop = function(){
+			if(this.running){
+				this.renderer.render(this.scene, this.camera);
+				requestAnimationFrame(this.loop.bind(this));
 			}
+		}
 	}
 );
 
 export let sqrt3_slide = new Slide(
-	function(domElement){
-			let width = domElement.parentElement.clientWidth;
-			let height = domElement.parentElement.clientHeight;
+	function(domElement, width, height){
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x191919);
+		this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
+		let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
+		pointLight0.position.set(10,8,5);
+		this.scene.add(pointLight0);
+		
+		this.renderer = new THREE.WebGLRenderer();	
+		this.renderer.setSize( width *0.8, height *0.8);
+		domElement.appendChild( this.renderer.domElement );
 
-			this.scene = new THREE.Scene();
-			this.scene.background = new THREE.Color(0x191919);
-			this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
-			let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
-			pointLight0.position.set(10,8,5);
-			this.scene.add(pointLight0);
-			
-			this.renderer = new THREE.WebGLRenderer();	
-			this.renderer.setSize( width *0.8, height *0.8);
-			domElement.appendChild( this.renderer.domElement );
+		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 2);
 
-			this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
-			this.camera.position.set(0, 0, 2);
+		let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
+		orbit_controls.enablePan = false;
+		orbit_controls.update();
 
-			let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
-			orbit_controls.enablePan = false;
-			orbit_controls.update();
+		
+		this.cmap2 = load_cmap2('off', octahedron_off);
+		this.map_renderer = new Renderer(this.cmap2);
+		this.map_renderer.vertices.create().add(this.scene);
+		this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
+		this.map_renderer.faces.create().add(this.scene);
 
-			
-			this.cmap2 = load_cmap2('off', octahedron_off);
-			this.map_renderer = new Renderer(this.cmap2);
-			this.map_renderer.vertices.create().add(this.scene);
-			this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
-			this.map_renderer.faces.create().add(this.scene);
-
-			function onKeyUp(event){
-				switch(event.which){
-					case 67:
-						sqrt3(this.cmap2);
-						this.map_renderer.vertices.update();
-						this.map_renderer.edges.update();
-						this.map_renderer.faces.update();
-						break;
-					default:
-				}
+		function onKeyUp(event){
+			switch(event.which){
+				case 67:
+					sqrt3(this.cmap2);
+					this.map_renderer.vertices.update();
+					this.map_renderer.edges.update();
+					this.map_renderer.faces.update();
+					break;
+				default:
 			}
-			domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
+		}
+		domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
 
-			this.loop = function(){
-				if(this.running){
-					this.renderer.render(this.scene, this.camera);
-					requestAnimationFrame(this.loop.bind(this));
-				}
+		this.loop = function(){
+			if(this.running){
+				this.renderer.render(this.scene, this.camera);
+				requestAnimationFrame(this.loop.bind(this));
 			}
+		}
 	}
 );
 
 export let loop_slide = new Slide(
-	function(domElement){
-			let width = domElement.parentElement.clientWidth;
-			let height = domElement.parentElement.clientHeight;
+	function(domElement, width, height){
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x191919);
+		this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
+		let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
+		pointLight0.position.set(10,8,5);
+		this.scene.add(pointLight0);
+		
+		this.renderer = new THREE.WebGLRenderer();	
+		this.renderer.setSize( width *0.8, height *0.8);
+		domElement.appendChild( this.renderer.domElement );
 
-			this.scene = new THREE.Scene();
-			this.scene.background = new THREE.Color(0x191919);
-			this.scene.add(new THREE.AmbientLight(0xAAAAFF, 0.8));
-			let pointLight0 = new THREE.PointLight(0x3137DD, 0.8);
-			pointLight0.position.set(10,8,5);
-			this.scene.add(pointLight0);
-			
-			this.renderer = new THREE.WebGLRenderer();	
-			this.renderer.setSize( width *0.8, height *0.8);
-			domElement.appendChild( this.renderer.domElement );
+		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 2);
 
-			this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000.0);
-			this.camera.position.set(0, 0, 2);
+		let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
+		orbit_controls.enablePan = false;
+		orbit_controls.update();
 
-			let orbit_controls = new OrbitControls(this.camera, this.renderer.domElement)
-			orbit_controls.enablePan = false;
-			orbit_controls.update();
+		
+		this.cmap2 = load_cmap2('off', octahedron_off);
+		this.map_renderer = new Renderer(this.cmap2);
+		this.map_renderer.vertices.create().add(this.scene);
+		this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
+		this.map_renderer.faces.create().add(this.scene);
 
-			
-			this.cmap2 = load_cmap2('off', octahedron_off);
-			this.map_renderer = new Renderer(this.cmap2);
-			this.map_renderer.vertices.create().add(this.scene);
-			this.map_renderer.edges.create({color: 0xFFFFFF}).add(this.scene);
-			this.map_renderer.faces.create().add(this.scene);
-
-			function onKeyUp(event){
-				switch(event.which){
-					case 67:
-						loop(this.cmap2);
-						this.map_renderer.vertices.update();
-						this.map_renderer.edges.update();
-						this.map_renderer.faces.update();
-						break;
-					default:
-				}
+		function onKeyUp(event){
+			switch(event.which){
+				case 67:
+					loop(this.cmap2);
+					this.map_renderer.vertices.update();
+					this.map_renderer.edges.update();
+					this.map_renderer.faces.update();
+					break;
+				default:
 			}
-			domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
+		}
+		domElement.addEventListener( 'keyup', onKeyUp.bind(this), false );
 
-			this.loop = function(){
-				if(this.running){
-					this.renderer.render(this.scene, this.camera);
-					requestAnimationFrame(this.loop.bind(this));
-				}
+		this.loop = function(){
+			if(this.running){
+				this.renderer.render(this.scene, this.camera);
+				requestAnimationFrame(this.loop.bind(this));
 			}
+		}
 	}
 );
