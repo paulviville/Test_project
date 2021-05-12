522a182d6435b66bb7150e642334accfebb4e460
paulviville <paul.viville@etu.unistra.fr>
2021-05-12 15:41:28 +0200

    imrpoved grid

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ scene.add(pointLight0);
 function Grid2D (params = {}) {
 	let { xmin = -1, xmax = 1, 
 		ymin = -1, ymax = 1,
-		xdivs = 10,	ydivs = 10 } = params;
+		xdivs = 2,	ydivs = 2 } = params;
 	console.log(xmin, xmax, ymin, ymax, xdivs, ydivs);
 
 	const map = new CMap2;
@@ -46,7 +46,7 @@ function Grid2D (params = {}) {
 
 	this.getVertex = function (i, j, v) {
 		let vd;
-		const d = getCell(i, j);
+		const d = this.getCell(i, j);
 		switch(v) {
 			case 1:
 				vd = map.phi1[d];
@@ -65,14 +65,31 @@ function Grid2D (params = {}) {
 
 	this.getEdge = this.getVertex;
 
-	function initGrid() {
+	this.initGrid = function () {
 		for(let i = 0; i < ydivs; ++i) {
 			for(let j = 0; j < xdivs; ++j) {
-				grid[j + i * xdivs] = map.addFace1(4);		
+				const fd00 = map.addFace1(4);
+				grid[j + i * xdivs] = fd00;
+				if(j > 0) {
+					const ed00 = this.getEdge(i, j, 3);
+					const ed10 = this.getEdge(i, j - 1, 1);
+					map.sew_phi2(ed00, ed10);
+				}
+				if(i > 0) {
+					const ed00 = this.getEdge(i, j, 0);
+					const ed10 = this.getEdge(i - 1, j, 2);
+					map.sew_phi2(ed00, ed10);
+				}
 			}
 		}
+
+		map.close();
+		console.log(map.nbCells(map.vertex), map.nbCells(map.edge), map.nbCells(map.face));
 	}
-	
+
+	this.initGrid();
+
+	return this;
 }
 
 
